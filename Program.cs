b9abf562276ab2d52272bb77eb9789fd3fbaf82d
Aakankshap16 <aakankshap971@gmail.com>
2023-04-26 13:31:04 +0530

    Create List of ProductReview

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
        public void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Review Managment Program!!!");
+
+            List<ProductReview> ProductReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID = 1,UserID = 10,Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID = 2,UserID = 11,Rating=4,Review="Nice",IsLike=true},
+                new ProductReview(){ProductID = 3,UserID = 12,Rating=3,Review="Average",IsLike=true},
+                new ProductReview(){ProductID = 4,UserID = 13,Rating=4,Review="Nice",IsLike=true},
+                new ProductReview(){ProductID = 5,UserID = 14,Rating=5,Review="Good",IsLike=true}
+
+            };
         }
     }
 }
