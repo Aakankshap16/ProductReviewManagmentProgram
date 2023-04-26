d5b47122eae1f13b4a06665ad01a3be57f83e9d1
Aakankshap16 <aakankshap971@gmail.com>
2023-04-26 22:08:43 +0530

    Create List of ProductReview and call

--- Program.cs
+++ Program.cs
@@ -9,16 +9,17 @@
             List<ProductReview> ProductReviewList = new List<ProductReview>()
             {
                 new ProductReview(){ProductID = 1,UserID = 10,Rating=5,Review="Good",IsLike=true},
-                new ProductReview(){ProductID = 2,UserID = 11,Rating=4,Review="Nice",IsLike=true},
+                new ProductReview(){ProductID = 1,UserID = 11,Rating=4,Review="Nice",IsLike=true},
                 new ProductReview(){ProductID = 3,UserID = 12,Rating=3,Review="Average",IsLike=true},
-                new ProductReview(){ProductID = 4,UserID = 13,Rating=4,Review="Nice",IsLike=true},
+                new ProductReview(){ProductID = 5,UserID = 13,Rating=4,Review="Nice",IsLike=true},
                 new ProductReview(){ProductID = 5 ,UserID = 14,Rating=5,Review="Good",IsLike=true}
 
             };
 
             Managemenent managemenent = new Managemenent();
             //managemenent.topRecords(ProductReviewList);
-            managemenent.SelectedRecords(ProductReviewList);
+            //managemenent.SelectedRecords(ProductReviewList);
+            managemenent.RetrieveCountOfRecords(ProductReviewList);
         }
     }
 }
