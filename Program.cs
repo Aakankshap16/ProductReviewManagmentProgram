5927f3489fc4d934ffba47bee9e40da440b90b82
Aakankshap16 <aakankshap971@gmail.com>
2023-04-26 18:11:02 +0530

    Create List of ProductReview and call

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
   public class Program
     {
-       public void Main(string[] args)
+       public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Product Review Managment Program!!!");
 
@@ -12,9 +12,12 @@
                 new ProductReview(){ProductID = 2,UserID = 11,Rating=4,Review="Nice",IsLike=true},
                 new ProductReview(){ProductID = 3,UserID = 12,Rating=3,Review="Average",IsLike=true},
                 new ProductReview(){ProductID = 4,UserID = 13,Rating=4,Review="Nice",IsLike=true},
-                new ProductReview(){ProductID = 5,UserID = 14,Rating=5,Review="Good",IsLike=true}
+                new ProductReview(){ProductID = 5 ,UserID = 14,Rating=5,Review="Good",IsLike=true}
 
             };
+
+            Managemenent managemenent = new Managemenent();
+            managemenent.topRecords(ProductReviewList);
         }
     }
 }
