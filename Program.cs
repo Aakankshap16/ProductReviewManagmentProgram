cf28083264d77e95abb674db1b691d6db10d791b
Aakankshap16 <aakankshap971@gmail.com>
2023-04-26 13:13:29 +0530

    Greeting Msg

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductReviewManagmentWithLinq
+{
+  public class Program
+    {
+       public void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Managment Program!!!");
+        }
+    }
+}
