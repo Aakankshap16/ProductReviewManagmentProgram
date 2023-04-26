6e269880ff453c51e19c20d557363f8f1b94a6f2
Aakankshap16 <aakankshap971@gmail.com>
2023-04-26 18:59:18 +0530

    code for find records with where clause

--- Managemenent.cs
+++ Managemenent.cs
@@ -8,6 +8,8 @@ namespace ProductReviewManagmentWithLinq
 {
     public class Managemenent
     {
+
+        // top 3 records
         public void topRecords(List<ProductReview> ListOfProductReview)
         {
             var RecordData = (from products in ListOfProductReview
@@ -18,6 +20,23 @@ namespace ProductReviewManagmentWithLinq
                 Console.Write("{0} {1} {2} {3} {4}\n", record.ProductID, record.UserID, record.Review, record.Rating, record.IsLike);
             }
 
+        }
+
+        //
+        public void SelectedRecords(List<ProductReview> ListOfProductReview)
+        {
+            var RecordData = from products in ListOfProductReview
+                             where (products.ProductID == 1 && products.Rating > 3) ||
+                                    (products.ProductID == 4 && products.Rating > 3) ||
+                                    (products.ProductID == 9 && products.Rating > 3)
+                             select products;
+
+
+            foreach (var record in RecordData)
+            {
+                Console.Write("{0} {1} {2} {3} {4}\n", record.ProductID, record.UserID, record.Review, record.Rating, record.IsLike);
+
+            }
         }
     }
 }
