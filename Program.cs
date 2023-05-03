e6ec5f10cbcb32c715a1a769f4f70050a2838426
Aakankshap16 <aakankshap971@gmail.com>
2023-05-04 01:23:37 +0530

    call the method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ProductReviewManagmentWithLinq
+﻿using System.Data;
+
+namespace ProductReviewManagmentWithLinq
 {
     public class Program
     {
@@ -22,7 +24,36 @@
             //managemenent.RetrieveCountOfRecords(ProductReviewList);
             //managemenent.retrieveID(ProductReviewList);
             // managemenent.SkipTop5Records(ProductReviewList);
-            managemenent.RetireveProductIDReview(ProductReviewList);
+            // managemenent.RetireveProductIDReview(ProductReviewList);
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ProductID", typeof(int));
+            dt.Columns.Add("UserID", typeof(int));
+            dt.Columns.Add("Rating", typeof(double));
+            dt.Columns.Add("Review", typeof(string));
+            dt.Columns.Add("IsLike", typeof(bool));
+
+            for (int i = 1; i <= 25; i++)
+            {
+                dt.Rows.Add(i, i + 10, i % 5 + 1, "Review " + i, i % 2 == 0);
+            }
+
+            // Retrieve records from DataTable using LINQ
+            var recordData = from row in dt.AsEnumerable()
+                             select new ProductReview
+                             {
+                                 ProductID = row.Field<int>("ProductID"),
+                                 UserID = row.Field<int>("UserID"),
+                                 Rating = row.Field<double>("Rating"),
+                                 Review = row.Field<string>("Review"),
+                                 IsLike = row.Field<bool>("IsLike")
+                             };
+
+            foreach (var record in recordData)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", record.ProductID, record.UserID, record.Rating, record.Review, record.IsLike);
+            }
         }
     }
 }
+  
